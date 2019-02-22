7f0a50a5e3d326cdba1bb485855a2abee1ad1b48
Michelle AVOMO <michelle.avomo@partnre.com>
2019-02-22 12:54:31 +0100

    better naming

--- src/test/java/fr/soat/cleancoders/ocp/bowling_kata/ScoringBowlingShould.java
+++ src/test/java/fr/soat/cleancoders/ocp/bowling_kata/ScoringBowlingShould.java
@@ -1,55 +1,44 @@
 package fr.soat.cleancoders.ocp.bowling_kata;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoringBowlingShould {
 
     private final BowlingGame bowlingGame = new BowlingGame();
 
-    private void rollSameNumberOfPinsTimes(int limit, int pinsDown) {
-        for (int i = 0; i < limit; i++) {
-            bowlingGame.roll(pinsDown);
-        }
-    }
-
-    private void rollSpareWith(int firstRound, int secondRoll) {
-        bowlingGame.roll(firstRound);
-        bowlingGame.roll(secondRoll);
-    }
-
     @Test
-    public void create_a_game_to_score_a_bowling_game() {
-        Assertions.assertThat(bowlingGame)
-                .as("should compile")
-                .isNotNull();
-    }
-
-    @Test
-    public void return_a_score_of_0_for_the_frame_when_no_pin_is_knocked_down_after_two_roll() {
+    public void return_a_score_of_0_for_a_frame_when_no_pin_is_knocked_down_after_two_roll() {
         rollSameNumberOfPinsTimes(2, 0);
 
-        Assertions.assertThat(bowlingGame.scoreOfFrame(2))
-                .as("no pins down")
+        final int scoreOfFrame = bowlingGame.scoreOfFrame(2);
+
+        assertThat(scoreOfFrame)
+                .as(" 0 pins down in two rolls")
                 .isEqualTo(0);
     }
 
     @Test
-    public void return_a_score_of_two_for_the_frame_when_1_pin_is_knocked_down_at_reach_roll() {
+    public void return_a_score_of_2_for_a_frame_when_1_pin_is_knocked_down_per_roll() {
         rollSameNumberOfPinsTimes(2, 1);
 
-        Assertions.assertThat(bowlingGame.scoreOfFrame(2))
-                .as("1 pin down for each roll")
+        final int scoreOfFrame = bowlingGame.scoreOfFrame(2);
+
+        assertThat(scoreOfFrame)
+                .as("1 pin down per roll")
                 .isEqualTo(2);
     }
 
     @Test
-    public void return_the_score_of_0_for_the_whole_game_when_gutter_games() {
+    public void return_the_score_of_0_for_the_whole_game_when_there_are_only_gutter_games() {
 
         rollSameNumberOfPinsTimes(20, 0);
 
-        Assertions.assertThat(bowlingGame.score())
-                .as("score is 0 because it was only gutter games ")
+        final int scoreOfTheGame = bowlingGame.score();
+
+        assertThat(scoreOfTheGame)
+                .as("score should be 0 because it was only gutter games ")
                 .isEqualTo(0);
     }
 
@@ -58,13 +47,15 @@ public class ScoringBowlingShould {
 
         rollSameNumberOfPinsTimes(20, 1);
 
-        Assertions.assertThat(bowlingGame.score())
+        final int scoreOfTheGame = bowlingGame.score();
+
+        assertThat(scoreOfTheGame)
                 .as("score is 0 because it was only gutter games ")
                 .isEqualTo(20);
     }
 
     @Test
-    public void add_the_number_of_pins_down_on_third_roll_when_there_is_a_spare() {
+    public void add_the_number_of_pins_down_in_the_first_roll_of_next_frame_when_scoring_frame_with_a_spare() {
         rollSpareWith(6, 4);
         bowlingGame.roll(4);
 
@@ -72,23 +63,21 @@ public class ScoringBowlingShould {
 
         final int scoreForFrame = bowlingGame.scoreOfFrame(0);
 
-        Assertions.assertThat(scoreForFrame)
+        assertThat(scoreForFrame)
                 .as("score is 14 it is a spare: R1  + R2 + R3 (bonus) = 6+4+4 = 14 pts ")
                 .isEqualTo(14);
     }
 
     @Test
-    public void add_the_first_roll_of_next_frame_to_current_frame_score_when_there_is_a_spare() {
+    public void count_the_spare_bonus_point_when_scoring_a_whole_game_with_a_spare() {
         rollSpareWith(6, 4);
-
         bowlingGame.roll(4);
         bowlingGame.roll(4);
-
         rollSameNumberOfPinsTimes(16, 0);
 
         final int scoreOfTheGame = bowlingGame.score();
 
-        Assertions.assertThat(scoreOfTheGame)
+        assertThat(scoreOfTheGame)
                 .as("score is 18 because:" +
                         " R1 + R2 is a spare. score (F1) =  R1  + R2 + R3 (bonus) = 6+4+4 = 14 pts " +
                         " F2 is a normal game = R3 + R4 = 8")
@@ -97,7 +86,7 @@ public class ScoringBowlingShould {
 
 
     @Test
-    public void add_the_two_rolls_of_next_frame_to_current_frame_score_when_there_is_a_strike() {
+    public void add_the_number_of_pins_down_in_the_first_2_rolls_of_next_frame_when_there_score_frame_with_a_strike() {
         rollAStrike();
         bowlingGame.roll(4);
         bowlingGame.roll(5);
@@ -106,14 +95,10 @@ public class ScoringBowlingShould {
 
         final int scoreOfTheGame = bowlingGame.scoreOfFrame(0);
 
-        Assertions.assertThat(scoreOfTheGame)
+        assertThat(scoreOfTheGame)
                 .as("score of the first frame is 19 because:" +
                         " R1 is a strike =  R1  + R2 + R3 (bonus) + R4 (bonus) = 10 + 4 + 5 = 19 pts ")
                 .isEqualTo(19);
-    }
-
-    private void rollAStrike() {
-        bowlingGame.roll(10);
     }
 
     @Test
@@ -126,7 +111,7 @@ public class ScoringBowlingShould {
 
         final int scoreOfTheGame = bowlingGame.score();
 
-        Assertions.assertThat(scoreOfTheGame)
+        assertThat(scoreOfTheGame)
                 .as("score of the first frame is 19 because:" +
                         " R1 is a strike =  R1  + R2 + R3 (bonus) + R4 (bonus) = 10 + 4 + 5 = 19 pts ")
                 .isEqualTo(28);
@@ -139,7 +124,25 @@ public class ScoringBowlingShould {
 
         final int scoreOfTheGame = bowlingGame.score();
 
-        Assertions.assertThat(scoreOfTheGame).isEqualTo(300);
+        assertThat(scoreOfTheGame).isEqualTo(300);
     }
 
+
+
+
+
+    private void rollSameNumberOfPinsTimes(int limit, int pinsDown) {
+        for (int i = 0; i < limit; i++) {
+            bowlingGame.roll(pinsDown);
+        }
+    }
+
+    private void rollSpareWith(int firstRound, int secondRoll) {
+        bowlingGame.roll(firstRound);
+        bowlingGame.roll(secondRoll);
+    }
+
+    private void rollAStrike() {
+        bowlingGame.roll(10);
+    }
 }
